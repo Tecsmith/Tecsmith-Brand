d0655b352124e2cf269334187a7e0c5d0234f29c
Vino Rodrigues <github@m.vinorodrigues.com>
2016-04-30 20:45:25 +1000

    muted the colors a bit

--- logo.svg.php
+++ logo.svg.php
@@ -4,17 +4,79 @@
  * @see: http://svgpocketguide.com/book  // SVG Ref
  */
 
+/**
+ * Color helper
+ */
+function brighten($color, $percentage) {
+	$steps = max(-10000, min(10000, $percentage*10000)) / 100;
+	$steps = round($percentage * 255);
+
+	$color = str_replace('#', '', $color);
+	if (strlen($color) == 3)
+        	$color = str_repeat(substr($color, 0, 1), 2) .
+        		str_repeat(substr($color, 1, 1), 2) .
+        		str_repeat(substr($color, 2, 1), 2);
+	if (strlen($color) != 6)
+		return false;
+	$RGB = str_split($color, 2);
+
+	$output = '#';
+	foreach ($RGB as $color) {
+		$color = hexdec($color);
+		$color = max( 0, min(255, $color + $steps) );
+		$output .= str_pad( dechex($color), 2, '0', STR_PAD_LEFT );
+	}
+	return $output;
+}
+
+function invert($color) {
+	$color = str_replace('#', '', $color);
+	if (strlen($color) == 3)
+        	$color = str_repeat(substr($color, 0, 1), 2) .
+        		str_repeat(substr($color, 1, 1), 2) .
+        		str_repeat(substr($color, 2, 1), 2);
+	if (strlen($color) != 6)
+		return false;
+	$RGB = str_split($color, 2);
+
+	$output = '#';
+	foreach ($RGB as $color) {
+		$color = hexdec($color);
+		$color = 255 - $color;
+		$output .= str_pad( dechex($color), 2, '0', STR_PAD_LEFT );
+	}
+	return $output;
+}
+
+/* ========== Starts here ================================================= */
+
 header('Content-type: image/svg+xml');
 header('Content-Disposition: inline; filename="' . basename(__FILE__, '.php') . '"');
 
 $req = array();
-$defaults = array('icon', 'avatar', 'nofill', 'guides', 'solid');
+$defaults = array('icon', 'avatar', 'nofill', 'guides', 'solid', 'inv');
 foreach ($defaults as $value)
 	$req[$value] = false;
 foreach ($_REQUEST as $key => $value)
 	if (in_array($key, $defaults))
 		$req[$key] = ($value == '') ? true : filter_var($value, FILTER_VALIDATE_BOOLEAN);
 extract($req);
+
+$black = '#000';  if ($inv) $black = invert($black);
+
+$red = '#c30000';
+$green = '#00c300';
+$blue = '#0000c3';
+
+$red_fr = '#fb0000';
+$red_to = '#8c0000';
+$green_fr = '#00fb00';
+$green_to = '#008c00';
+$blue_fr = '#0000fb';
+$blue_to = '#00008c';
+
+$red_alt = '#f03b4d';
+$black_alt = '#565656';
 
 $fill = false;
 $color = false;
@@ -35,40 +97,22 @@ if (isset($_REQUEST['fill'])) {
 	}
 }
 
+$size = false;
+if (isset($_REQUEST['size']) && ($_REQUEST['size'] != ''))
+	$size = round($_REQUEST['size']);
+if ($size !== false) {
+	$size = round($size);
+	if ($size <= 0) $size = false;
+}
+if ($size === false) $size = 1024;
 
-$w = $h = 1024;
-$s = intval($w / 50);
-$s = 40;
+
+$w = $h = $size;
+$s = round($w / 25);
+
 
 global $add_x, $add_y;
 $add_x = $add_y = 0;
-
-/**
- * Color helper
- */
-function brighten($color, $percentage) {
-	$steps = max(-10000, min(10000, $percentage*10000)) / 100;
-	$steps = intval($percentage * 255);
-
-	$color = str_replace('#', '', $color);
-	if (strlen($color) == 3)
-        	$color = str_repeat(substr($color, 0, 1), 2) .
-        		str_repeat(substr($color, 1, 1), 2) .
-        		str_repeat(substr($color, 2, 1), 2);
-
-        if (strlen($color) != 6)
-        	return false;
-
-	$RGB = str_split($color, 2);
-	$output = '#';
-
-	foreach ($RGB as $color) {
-		$color = hexdec($color);
-		$color = max( 0, min(255, $color + $steps) );
-		$output .= str_pad( dechex($color), 2, '0', STR_PAD_LEFT );
-	}
-	return $output;
-}
 
 
 /**
@@ -82,13 +126,13 @@ function c($x, $y, $xo = 0, $yo = 0, $end = ' ') {
 function side_t($w, $h, $s) {
 	$out = '';
 
-	$o = intval($s*2);
+	$o = round($s*2);
 	$w = $w-($o*2);
 	$h = $h-($o*2);
 
 	$a = atan( ($w / 2) / ($h / 4) );
 	$x0 = $o;
-	$y0 = $o - intval(sin($a) * $s);
+	$y0 = $o - round(sin($a) * $s);
 
 	$iw = $w / 2;
 	$ih = $h / 4;
@@ -107,13 +151,13 @@ function side_t($w, $h, $s) {
 function side_r($w, $h, $s) {
 	$out = '';
 
-	$o = intval($s*2);
+	$o = round($s*2);
 	$w = $w-($o*2);
 	$h = $h-($o*2);
 
 	$a = atan( ($w / 2) / ($h / 4) );
 	$x0 = $o + $s;
-	$y0 = $o + intval(sin($a) * $s);
+	$y0 = $o + round(sin($a) * $s);
 
 	$iw = $w / 2;
 	$ih = $h / 4;
@@ -132,13 +176,13 @@ function side_r($w, $h, $s) {
 function side_l($w, $h, $s) {
 	$out = '';
 
-	$o = intval($s*2);
+	$o = round($s*2);
 	$w = $w-($o*2);
 	$h = $h-($o*2);
 
 	$a = atan( ($w / 2) / ($h / 4) );
 	$x0 = $o - $s;
-	$y0 = $o + intval(sin($a) * $s);
+	$y0 = $o + round(sin($a) * $s);
 
 	$iw = $w / 2;
 	$ih = $h / 4;
@@ -175,13 +219,13 @@ function f($cs1, $cs2, $gg1, $gg2, $cst, $sld) {
 if ($icon) {
 	$width = $w;
 } elseif ($avatar) {
-	$width = intval($w * 1.5);
-	$add_x = intval($w / 4);
+	$width = round($w * 1.5);
+	$add_x = round($w / 4);
 } else {
-	$width = $w + intval($w * 4.73);
+	$width = $w + round($w * 4.74);
 }
 if ($avatar) {
-	$height = intval($h * 1.5);
+	$height = round($h * 1.5);
 	$add_y = ($h * 0.1);
 } else {
 	$height = $h;
@@ -199,16 +243,16 @@ if ($avatar) {
 	<?php } elseif (!$nofill && !$solid) { ?>
 	<defs>
 		<linearGradient id="grad-red" x1="0%" y1="0%" x2="100%" y2="0%">
-			<stop offset="0%" style="stop-color: <?= brighten('#f00', 0.33) ?>; stop-opacity: 1" />
-			<stop offset="100%" style="stop-color: <?= brighten('#f00', -0.33) ?>; stop-opacity: 1" />
+			<stop offset="0%" style="stop-color: <?= $red_fr ?>; stop-opacity: 1" />
+			<stop offset="100%" style="stop-color: <?= $red_to ?>; stop-opacity: 1" />
 		</linearGradient>
 		<linearGradient id="grad-green" x1="0%" y1="0%" x2="100%" y2="0%">
-			<stop offset="0%" style="stop-color: <?= brighten('#0f0', 0.33) ?>; stop-opacity: 1" />
-			<stop offset="100%" style="stop-color: <?= brighten('#0f0', -0.33) ?>; stop-opacity: 1" />
+			<stop offset="0%" style="stop-color: <?= $green_fr ?>; stop-opacity: 1" />
+			<stop offset="100%" style="stop-color: <?= $green_to ?>; stop-opacity: 1" />
 		</linearGradient>
 		<linearGradient id="grad-blue" x1="0%" y1="0%" x2="100%" y2="0%">
-			<stop offset="0%" style="stop-color: <?= brighten('#00f', 0.33) ?>; stop-opacity: 1" />
-			<stop offset="100%" style="stop-color: <?= brighten('#00f', -0.33) ?>; stop-opacity: 1" />
+			<stop offset="0%" style="stop-color: <?= $blue_fr ?>; stop-opacity: 1" />
+			<stop offset="100%" style="stop-color: <?= $blue_to ?>; stop-opacity: 1" />
 		</linearGradient>
 	</defs>
 	<?php } ?>
@@ -228,15 +272,15 @@ if ($avatar) {
 	<g id="filler">
 		<g id="filler-top">
 			<title>Top-Frame</title>
-			<polygon stroke="red" id="poly-ft" style="stroke-width: 1; fill: <?= f('red', $color, 'grad-red', 'grad-fill', $fill, $solid) ?>; stroke-linejoin: round; stroke-linecap: round;" points="<?= side_t($w, $h, $s) ?>"/>
+			<polygon stroke="<?= $red ?>" id="poly-ft" style="stroke-width: 1; fill: <?= f($red, $color, 'grad-red', 'grad-fill', $fill, $solid) ?>; stroke-linejoin: round; stroke-linecap: round;" points="<?= side_t($w, $h, $s) ?>"/>
 		</g>
 		<g id="filler-right">
 			<title>Right-Frame</title>
-			<polygon stroke="blue" id="poly-fr" style="stroke-width: 1; fill: <?= f('blue', $color, 'grad-blue', 'grad-fill', $fill, $solid) ?>; stroke-linejoin: round; stroke-linecap: round;" points="<?= side_r($w, $h, $s) ?>"/>
+			<polygon stroke="<?= $blue ?>" id="poly-fr" style="stroke-width: 1; fill: <?= f($blue, $color, 'grad-blue', 'grad-fill', $fill, $solid) ?>; stroke-linejoin: round; stroke-linecap: round;" points="<?= side_r($w, $h, $s) ?>"/>
 		</g>
 		<g id="filler-left">
 				<title>Left-Frame</title>
-			<polygon stroke="green" id="poly-fl" style="stroke-width: 1; fill: <?= f('green', $color, 'grad-green', 'grad-fill', $fill, $solid) ?>; stroke-linejoin: round; stroke-linecap: round;" points="<?= side_l($w, $h, $s) ?>"/>
+			<polygon stroke="<?= $green ?>" id="poly-fl" style="stroke-width: 1; fill: <?= f($green, $color, 'grad-green', 'grad-fill', $fill, $solid) ?>; stroke-linejoin: round; stroke-linecap: round;" points="<?= side_l($w, $h, $s) ?>"/>
 		</g>
 	</g>
 	<?php } ?>
@@ -244,29 +288,29 @@ if ($avatar) {
 	<g id="frame">
 		<g id="frame-top">
 			<title>Top-Frame</title>
-			<polygon stroke="black" id="poly-ft" style="stroke-width: <?= $s ?>; fill: none; stroke-linejoin: round; stroke-linecap: round;" points="<?= side_t($w, $h, $s) ?>"/>
+			<polygon stroke="<?= $black ?>" id="poly-ft" style="stroke-width: <?= $s ?>; fill: none; stroke-linejoin: round; stroke-linecap: round;" points="<?= side_t($w, $h, $s) ?>"/>
 		</g>
 		<g id="frame-right">
 			<title>Right-Frame</title>
-			<polygon stroke="black" id="poly-fr" style="stroke-width: <?= $s ?>; fill: none; stroke-linejoin: round; stroke-linecap: round;" points="<?= side_r($w, $h, $s) ?>"/>
+			<polygon stroke="<?= $black ?>" id="poly-fr" style="stroke-width: <?= $s ?>; fill: none; stroke-linejoin: round; stroke-linecap: round;" points="<?= side_r($w, $h, $s) ?>"/>
 		</g>
 		<g id="frame-left">
 			<title>Left-Frame</title>
-			<polygon stroke="black" id="poly-fl" style="stroke-width: <?= $s ?>; fill: none; stroke-linejoin: round; stroke-linecap: round;" points="<?= side_l($w, $h, $s) ?>"/>
+			<polygon stroke="<?= $black ?>" id="poly-fl" style="stroke-width: <?= $s ?>; fill: none; stroke-linejoin: round; stroke-linecap: round;" points="<?= side_l($w, $h, $s) ?>"/>
 		</g>
 	</g>
 
 	<?php if (!$icon && !$avatar) { ?>
 		<g id="word">
 		<?php if (!$nofill) { ?>
-			<text x="<?= $w + ($w / 6) ?>" y="<?= intval($h * 0.9) ?>" fill="#f03b4d" font-size="<?= intval($h * 1.1) ?>" stroke="#f03b4d" stroke-width="<?= intval($s / 2) ?>" font-family="Ubuntu" font-weight="bold">tec<tspan fill="#565656" stroke="#565656">smith</tspan></text>
+			<text x="<?= $w + ($w / 6) ?>" y="<?= round($h * 0.9) ?>" fill="<?= $red_alt ?>" font-size="<?= round($h * 1.1) ?>" stroke="<?= $red_alt ?>" stroke-width="<?= round($s / 2) ?>" font-family="Ubuntu" font-weight="bold">tec<tspan fill="<?= $black_alt ?>" stroke="<?= $black_alt ?>">smith</tspan></text>
 		<?php } else { ?>
-			<text x="<?= $w + ($w / 6) ?>" y="<?= intval($h * 0.9) ?>" fill="none" stroke="black" stroke-width="<?= $s ?>" font-size="<?= intval($h * 1.1) ?>" font-family="Ubuntu" font-weight="bold">tecsmith</text>
+			<text x="<?= $w + ($w / 6) ?>" y="<?= round($h * 0.9) ?>" fill="none" stroke="<?= $black ?>" stroke-width="<?= $s ?>" font-size="<?= round($h * 1.1) ?>" font-family="Ubuntu" font-weight="bold">tecsmith</text>
 		<?php }  // $nofill ?>
 		</g>
 	<?php } elseif ($avatar) { ?>
 		<g id="word">
-			<text x="<?= $s ?>" y="<?= intval($h * 1.35) ?>" fill="#f03b4d" font-size="<?= intval($h * 0.34) ?>" font-family="Ubuntu" font-weight="bold">tec<tspan fill="#565656" stroke="#565656">smith</tspan></text>
+			<text x="<?= $s ?>" y="<?= round($h * 1.35) ?>" fill="<?= $red_alt ?>" font-size="<?= round($h * 0.34) ?>" font-family="Ubuntu" font-weight="bold">tec<tspan fill="<?= $black_alt ?>">smith</tspan></text>
 		</g>
 	<?php }  ?>
 
